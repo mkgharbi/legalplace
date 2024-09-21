3422f22a025ff1d3f42f0516d0cbb29d6f0f4385
Mohamed Khairallah Gharbi <mohamed.khairallah.gharbi@gmail.com>
2024-09-21 16:37:21 +0200

    add dafalgan case

--- pharmacy.js
+++ pharmacy.js
@@ -18,7 +18,12 @@ export class Pharmacy {
       ) {
         if (this.drugs[i].benefit > 0) {
           if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
+            if (this.drugs[i].name == "Dafalgan") {
+              this.drugs[i].benefit =
+                this.drugs[i].benefit - 2 < 0 ? 0 : this.drugs[i].benefit - 2;
+            } else {
+              this.drugs[i].benefit = this.drugs[i].benefit - 1;
+            }
           }
         }
       } else {
@@ -49,8 +54,6 @@ export class Pharmacy {
                 this.drugs[i].benefit = this.drugs[i].benefit - 1;
               }
             }
-          // } else if () {
-
           } else {
             this.drugs[i].benefit =
               this.drugs[i].benefit - this.drugs[i].benefit;
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -6,4 +6,15 @@ describe("Pharmacy", () => {
       [new Drug("test", 1, 2)],
     );
   });
+
+  it("should decrease the benefit twice as fast when expired", () => {
+    expect(
+      new Pharmacy([new Drug("Dafalgan", 2, 3)]).updateBenefitValue(),
+    ).toEqual([new Drug("Dafalgan", 1, 1)]);
+  });
+  it("should increase the benefit of Herbal Tea", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue(),
+    ).toEqual([new Drug("Herbal Tea", 1, 4)]);
+  });
 });
